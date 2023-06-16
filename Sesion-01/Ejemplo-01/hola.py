7dcbeef3ef387f929b64af68ba8c93ca168d8baf
rctorr <rictor@cuhrt.com>
2023-06-15 18:24:41 -0600

    Agregando código para Sesion03

---
+++ Sesion-01/Ejemplo-01/hola.py
@@ -0,0 +1,12 @@
+def saludo():
+    print("Hola")
+
+def main():
+    print("Que la fuerza de python te acompañe!")
+    print("cuidado con la identanción!")
+
+
+if __name__ == "__main__":
+    main()
+    saludo()
+
